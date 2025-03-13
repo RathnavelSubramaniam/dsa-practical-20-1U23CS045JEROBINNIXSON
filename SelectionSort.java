58a04d9d333efa9932f34a5b805a9b849146ef53
1U23CS045JEROBINNIXSON <139536670+1U23CS045JEROBINNIXSON@users.noreply.github.com>
2025-03-13 06:45:04 +0000

    20

--- SelectionSort.java
+++ SelectionSort.java
@@ -1,37 +1,52 @@
 import java.util.Scanner;
-
 public class SelectionSort {
-    public static void selectionSortIterative() {
-        //TYPE YOUR CODE HERE
+ public static void selectionSortIterative(int[] arr) {
+ int n = arr.length;
+ for (int i = 0; i < n
+- 1; i++) {
+ int minIndex = i;
+ for (int j = i + 1; j < n; j++) {
+ if (arr[j] < arr[minIndex]) {
+ minIndex = j;
 
-            System.out.print("Array after iteration " + (i + 1) + ": ");
-            for (int k = 0; k < arr.length; k++) {
-                System.out.print(arr[k] + " ");
-            }
-            System.out.println();
-        }
-    }
-
-    public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        System.out.print("Enter the number of elements: ");
-        int size = scanner.nextInt();
-        int[] array = new int[size];
-        System.out.println("Enter the elements:");
-        for (int i = 0; i < size; i++) {
-            array[i] = scanner.nextInt();
-        }
-        System.out.print("Array before sorting: ");
-        for (int i = 0; i < array.length; i++) {
-            System.out.print(array[i] + " ");
-        }
-        System.out.println();
-        selectionSortIterative(array);
-        System.out.print("Array after sorting: ");
-        for (int i = 0; i < array.length; i++) {
-            System.out.print(array[i] + " ");
-        }
-        System.out.println();
-    }
 }
 
+}
+ int temp = arr[minIndex];
+ arr[minIndex] = arr[i];
+ arr[i] = temp;
+ System.out.print("Array after iteration " + (i + 1) + ": ");
+ for (int k = 0; k < arr.length; k++) {
+ System.out.print(arr[k] + " ");
+
+}
+ System.out.println();
+
+}
+}
+ public static void main(String[] args) {
+ Scanner scanner = new Scanner(System.in);
+ System.out.println("Enter the number of elements: ");
+ int size = scanner.nextInt();
+ int[] array = new int[size];
+ System.out.println("Enter the elements:");
+ for (int i = 0; i < size; i++) {
+ array[i] = scanner.nextInt();
+
+}
+ System.out.print("Array before sorting: ");
+ for (int i = 0; i < array.length; i++) {
+ System.out.print(array[i] + " ");
+
+}
+System.out.println();
+ selectionSortIterative(array);
+ System.out.print("Array after sorting: ");
+ for (int i = 0; i < array.length; i++) {
+ System.out.print(array[i] + " ");
+
+}
+ System.out.println();
+
+ }
+}
